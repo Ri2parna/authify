3f3ccec4c0eecb67f54a2c0c88341ad20710deb9
ri2parna <ri2parna@outlook.com>
2021-01-01 23:19:02 +0530

    refactor(): removed some endpoints

--- src/app.js
+++ src/app.js
@@ -6,19 +6,16 @@ const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // middleware
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(cookieParser());
-// view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
 
+// view engine
 // database connection
-const dbURI =
-  'mongodb+srv://authify-express-backend:iv8j9knyesvDLf13@notedlycluster.flnto.mongodb.net/authify?retryWrites=true&w=majority';
+const dbURI = process.env.CONNECTION_STRING;
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
